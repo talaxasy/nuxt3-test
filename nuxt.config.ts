6c913941ce1e2380983badc3beec0328c811c1c3
talaxasy <talaxasy.or.lev@gmail.com>
2022-08-01 14:32:35 +0300

    move all stuff into the ./src

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,18 @@
 import {defineNuxtConfig} from 'nuxt';
+import {resolve} from 'node:path';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig(
   {
     typescript: {
       shim: false,
+    },
+    srcDir: './src',
+    alias: {
+      '@': resolve(
+        __dirname,
+        'src',
+      ),
     },
   },
 );
---
+++ src/pages/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+
+</script>
+
+
+<template>
+    <div>
+        <h1>Index page</h1>
+    </div>
+</template>
